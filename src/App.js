a3a57aa9611efc3e989f16d5bac3243935a24f9a
jayStackk <jmagpantay@codestack.co>
2023-03-14 22:27:36 -0700

    Peer REview Push

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import QuoteBox from './body/AdviceBox';
 
+// Review: John's app looks really good, very similar style to the master copy with some deviations that are hard to notice. the color scheme and the blocking looks really good and the responsivnes works well. The only issue that i noticed was with the image on mobile
+// peer review by : [ Dan Decoito ]
 function App() {
 
   return (
